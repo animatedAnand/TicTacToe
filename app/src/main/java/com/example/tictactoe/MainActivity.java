b4c03814ddb5d04894706305ffb5d5b7008ca5c5
anand <kumar.abhayanand19@gmail.com>
2023-02-14 17:51:02 +0530

    Constraint layout implemented. Game drawn signal added.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -13,12 +13,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button bt0,bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt_reset_game;
+    Button bt0, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt_reset_game;
     CardView cv_header;
     TextView tv_player_turn;
-    int[] array={0,0,0,0,0,0,0,0,0};
-    int[][] winningCombo={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    int current_player=0,winner=0;
+    int[] array = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int[][] winningCombo = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    int current_player = 0, winner = 0,tap_count=0;
     Button[] btArray;
 
     @Override
@@ -26,19 +26,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt0=findViewById(R.id.bt0);
-        bt1=findViewById(R.id.bt1);
-        bt2=findViewById(R.id.bt2);
-        bt3=findViewById(R.id.bt3);
-        bt4=findViewById(R.id.bt4);
-        bt5=findViewById(R.id.bt5);
-        bt6=findViewById(R.id.bt6);
-        bt7=findViewById(R.id.bt7);
-        bt8=findViewById(R.id.bt8);
+        bt0 = findViewById(R.id.bt0);
+        bt1 = findViewById(R.id.bt1);
+        bt2 = findViewById(R.id.bt2);
+        bt3 = findViewById(R.id.bt3);
+        bt4 = findViewById(R.id.bt4);
+        bt5 = findViewById(R.id.bt5);
+        bt6 = findViewById(R.id.bt6);
+        bt7 = findViewById(R.id.bt7);
+        bt8 = findViewById(R.id.bt8);
 
-        bt_reset_game=findViewById(R.id.bt_reset_game);
-        cv_header=findViewById(R.id.cv_tic_tac_toe);
-        tv_player_turn=findViewById(R.id.tv_player_turn);
+        bt_reset_game = findViewById(R.id.bt_reset_game);
+        cv_header = findViewById(R.id.cv_tic_tac_toe);
+        tv_player_turn = findViewById(R.id.tv_player_turn);
 
         bt0.setOnClickListener(this);
         bt1.setOnClickListener(this);
@@ -50,17 +50,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt7.setOnClickListener(this);
         bt8.setOnClickListener(this);
 
-        btArray = new Button[]{bt0,bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8};
+        btArray = new Button[]{bt0, bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8};
         bt_reset_game.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tv_player_turn.setText("Player O turn");
-                current_player=0;
-                winner=0;
-                for(int i=0;i<9;i++)
-                {
+                current_player = 0;
+                winner = 0;
+                tap_count=0;
+                for (int i = 0; i < 9; i++) {
                     btArray[i].setText("");
-                    array[i]=0;
+                    array[i] = 0;
                     btArray[i].setBackgroundColor(Color.parseColor("#FF6200EE"));
                 }
             }
@@ -69,45 +69,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Button current_button=findViewById(v.getId());
-        int cbv=Integer.parseInt(v.getTag().toString());
-        if(array[cbv]==0)
-        {
-           if(current_player==0)
-           {
-               current_button.setText("O");
-               array[cbv]=1;
-               if(winner==0)
-               winner=showResponse(0);
-               if(winner==0) tv_player_turn.setText("Player X turn");
-               current_player=1;
-           }
-           else
-           {
-               current_button.setText("X");
-               array[cbv]=2;
-               if(winner==0)
-               winner=showResponse(1);
-               if(winner==0) tv_player_turn.setText("Player O turn");
-               current_player=0;
-           }
+        Button current_button = findViewById(v.getId());
+        int cbv = Integer.parseInt(v.getTag().toString());
+        if (array[cbv] == 0) {
+            tap_count++;
+            if (current_player == 0) {
+                current_button.setText("O");
+                array[cbv] = 1;
+                if (winner == 0)
+                    winner = showResponse(0);
+                if (winner == 0 && tap_count<9) tv_player_turn.setText("Player X turn");
+                current_player = 1;
+            } else {
+                current_button.setText("X");
+                array[cbv] = 2;
+                if (winner == 0)
+                    winner = showResponse(1);
+                if (winner == 0 && tap_count<9) tv_player_turn.setText("Player O turn");
+                current_player = 0;
+            }
         }
     }
 
     private int showResponse(int current_player) {
-        int cur_win=0;
-        for(int i=0;i<8;i++)
-        {
-            if((array[winningCombo[i][0]]>0) && (array[winningCombo[i][0]]==array[winningCombo[i][1]])
-                    && (array[winningCombo[i][0]]==array[winningCombo[i][2]]))
-            {
-                if(current_player==0) {tv_player_turn.setText("Player O won!"); cur_win=1;}
-                else {tv_player_turn.setText("Player X won!"); cur_win=2;}
+        int cur_win = 0;
+        for (int i = 0; i < 8; i++) {
+            if ((array[winningCombo[i][0]] > 0) && (array[winningCombo[i][0]] == array[winningCombo[i][1]])
+                    && (array[winningCombo[i][0]] == array[winningCombo[i][2]])) {
+                if (current_player == 0) {
+                    tv_player_turn.setText("Player O won!");
+                    cur_win = 1;
+                } else {
+                    tv_player_turn.setText("Player X won!");
+                    cur_win = 2;
+                }
                 btArray[winningCombo[i][0]].setBackgroundColor(Color.parseColor("#A62B2B"));
                 btArray[winningCombo[i][1]].setBackgroundColor(Color.parseColor("#A62B2B"));
                 btArray[winningCombo[i][2]].setBackgroundColor(Color.parseColor("#A62B2B"));
                 return cur_win;
             }
+            if(tap_count==9) tv_player_turn.setText("Game drawn!");
         }
         return 0;
     }
